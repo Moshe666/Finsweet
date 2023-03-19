70a6deb4f8b3af06540a1145068d801557585ba7
Moshe666 <125701392+Moshe666@users.noreply.github.com>
2023-03-19 17:32:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(function () {
 
+// ============================ RATE ===============================
+
   $(".header__rateYo").rateYo({
     rating: 4.5,
     spacing: "5px",
@@ -11,6 +13,8 @@ $(function () {
   $(".header__rateYo").rateYo("option", "ratedFill", "#FFB648");
 
   var mixer = mixitup('.blog__list');
+
+  // ==================== BLOG =======================
 
   $('.blog__filter-btn').on('click', function () {
     $('.blog__filter-btn').removeClass('blog__filter-item--active')
@@ -25,17 +29,36 @@ $(function () {
     draggable: false,
     dots: true,
     appendDots: $('.customers__slider-buttons'),
-    
-  })
 
+  })
+   // ============================= SLAIDER =============================
   $('.customers__slider-prev').on('click', function (e) {
     e.preventDefault()
     $('.customers__slider').slick('slickPrev')
-})
-$('.customers__slider-next').on('click', function (e) {
+  })
+  $('.customers__slider-next').on('click', function (e) {
     e.preventDefault()
     $('.customers__slider').slick('slickNext')
-})
+  })
+
+  // ============================= ACCORDEON ======================
+
+  $('.questions__acc-link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('questions__acc-link--active')) {
+      $(this).removeClass('questions__acc-link--active')
+      $(this).children('.questions__acc-text').slideUp()
+    } else {
+      $('.questions__acc-link').removeClass('questions__acc-link--active')
+      $('.questions__acc-text').slideUp()
+      $(this).addClass('questions__acc-link--active')
+      $(this).children('.questions__acc-text').slideDown()
+    }
+  })
+
+
+
+
 
 });
 
